6d5889e5e4756d81196b451f8af4a8da99bd9f9f
rgerum <14153051+rgerum@users.noreply.github.com>
2023-04-24 12:10:04 -0400

    main.py added terminal flush

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Logger(object):
         # this flush method is needed for python 3 compatibility.
         # this handles the flush command by doing nothing.
         # you might want to specify some extra behavior here.
+        self.terminal.flush()
         pass
 
 
